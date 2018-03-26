c6631366225c2234ce1e8b3bcc19904cb8f7afde
Chiamaka Ikeanyi <chiamakaikeanyi@gmail.com>
2018-03-26 16:41:55 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Note from './components/Note';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      noteText: '',
+      notes: [],
+    }
+  }
+
+  addNote() {
+    let notesArr = this.state.notes;
+    notesArr.push(this.state.noteText);
+    this.setState({ noteText: '' });
+    this.textInput.focus();
+  }
+
+  updateNoteText(noteText) {
+    this.setState({ noteText: noteText.target.value })
+  }
+
+  deleteNote(index) {
+    let notesArr = this.state.notes;
+    notesArr.splice(index, 1);
+    this.setState({ notes: notesArr });
+  }
+
+  handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      /*the mouse is automatically focused when enter key is pressed 
+        so there is no need for this.textInput.focus();
+      */
+      let notesArr = this.state.notes;
+      notesArr.push(this.state.noteText);
+      this.setState({ noteText: '' });
+    }
+  }
+
+  render() {
+
+    let notes = this.state.notes.map((val, key) => {
+      if (val === '') {
+        return
+      }
+      else {
+        return <Note key={key} text={val} deleteMethod={() => this.deleteNote(key)} />
+      }
+    });
+
+    return (
+      <div className="container">
+        <div className="header">
+          <img src={logo} className="App-logo" alt="logo" />
+          Task Manager
+        </div>
+
+        {notes}
+        
+        <div className="btn" onClick={this.addNote.bind(this)}>+</div>
+
+        <input type="text" t
+          ref={((input) => { this.textInput = input })}
+          className="textInput"
+          value={this.state.noteText}
+          onChange={noteText => this.updateNoteText(noteText)}
+          onKeyPress={this.handleKeyPress.bind(this)} />
+      </div>
+    );
+  }
+}
+
+export default App;
